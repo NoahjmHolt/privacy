9534cda9a2c5c6bba06e669d828d56ba40ad5f7a
Noah Holt <143626243+NoahjmHolt@users.noreply.github.com>
2024-02-26 14:00:28 -0700

    Removed filler
    Removed filler functions so I may begin to work.
    
    Also made not a test file (it is a test file)

--- HW3/main.py
+++ HW3/main.py
@@ -5,7 +5,6 @@
 # Noah Holt
 
 '''
-
 Why developed and security principles used?
 
 
@@ -13,13 +12,3 @@ Why developed and security principles used?
 '''
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
